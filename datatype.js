76a9179e2b77fa5a9b08bca2f0613560b0dcb8a7
Anurag Vishwakarma <76321314+A9169@users.noreply.github.com>
2024-10-24 10:41:03 +0000

    datatype

---
+++ datatype.js
@@ -0,0 +1,12 @@
+"use strict"
+
+// alert(3+3)
+console.log(3+3)
+
+let name = "Anurag"
+let age = 81
+let islogedIn = false
+
+console.log(typeof "Anurag");
+console.log(typeof null);
+console.log(typeof undefined);
