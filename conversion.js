25e5b1f1635500e43bc91362b277aff295726061
Anurag Vishwakarma <76321314+A9169@users.noreply.github.com>
2024-10-25 09:57:49 +0000

    conversion

---
+++ conversion.js
@@ -0,0 +1,17 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let valueInboolean = Boolean(score);
+console.log(typeof valueInboolean);
+console.log(valueInboolean);
+
+let valueInString = String(score);
+console.log(typeof valueInString);
+console.log(valueInString);
+
