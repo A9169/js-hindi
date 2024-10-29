4a5d55bba0bf050bf1b283ca1c7c0fa23b35686e
Anurag Vishwakarma <76321314+A9169@users.noreply.github.com>
2024-10-29 10:28:55 +0000

    Heapstack

---
+++ heapstak.js
@@ -0,0 +1,22 @@
+let name1 = "Anurag Vishwakarma"
+
+let name2 = name1
+name2 = "is he good guy?"
+
+console.log(name1);
+console.log(name2);
+
+let userOne = {
+   email: "india@google.com" ,
+   upi : "user@ybl"
+  }
+
+let userTwo = userOne
+
+userTwo.email = "google@india.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
